2d95aeaa0122d45dd68d2f50722a8386d8950414
Acer <oone3136@gmail.com>
2024-02-26 04:26:53 +0700

    penggantian query

--- src/main/java/com/test/hilman/repository/MonitoringRepository.java
+++ src/main/java/com/test/hilman/repository/MonitoringRepository.java
@@ -9,11 +9,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MonitoringRepository extends JpaRepository<Monitoring, Long> {
-    @Query(value = "SELECT * FROM monitoring WHERE " +
-            "(:nama_lengkap IS NULL OR LOWER(nama_lengkap) " +
-            "LIKE LOWER(CONCAT('%', :nama_lengkap, '%'))) AND " +
-            "(:nik IS NULL OR nik = :nik);",
-            nativeQuery = true)
+//    @Query(value = "SELECT * FROM monitoring WHERE " +
+//            "(:nama_lengkap IS NULL OR LOWER(nama_lengkap) LIKE LOWER(CONCAT('%', :nama_lengkap, '%'))) AND " +
+//            "(:nik IS NULL OR nik = :nik);",
+//            nativeQuery = true)
+    @Query(value = "SELECT * " +
+            "FROM monitoring " +
+            "WHERE " +
+            "    (:nama_lengkap IS NULL OR LOWER(nama_lengkap) LIKE LOWER(CONCAT('%', :nama_lengkap, '%'))) AND \n" +
+            "    (:nik IS NULL OR CAST(nik AS VARCHAR) LIKE CONCAT('%', CAST(:nik AS VARCHAR), '%'));",
+    nativeQuery = true)
     List<Monitoring> searchByNikOrNamaLengkap(@Param("nama_lengkap") String nama,
                                               @Param("nik") Long nik);
 
